4426c8b4090f74df73975927e93dd91a2dbb07ac
hasanonik <jh8539615@gmail.com>
2024-04-18 12:22:47 +0600

    updated hero section

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -17,24 +17,42 @@ const Slider = () => {
             pagination={{ clickable: true }}>
             <SwiperSlide>
                 <div className='slider-1 slider'>
-                    <div className='overlay text-white flex items-center justify-center'>
-                        <h1 className='text-5xl'>Jahid hasa</h1>
+                    <div className='overlay flex items-center justify-center'>
+                        <div className='text-white text-center max-w-[700px] mx-auto'>
+                            <h1 className='text-5xl'>Your Journey to the Perfect Residence Starts Here</h1>
+                            <p className='text-lg mt-5'>Stylish apartments with cutting-edge design, luxurious amenities, and prime city location for an elevated urban lifestyle experience.</p>
+                        </div>
                     </div>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div className='slider-2 slider'>
-                    <div className='overlay'></div>
+                    <div className='overlay flex items-center justify-center'>
+                        <div className='text-white text-center max-w-[700px] mx-auto'>
+                            <h1 className='text-5xl'>Your Ultimate Guide to Residential Bliss</h1>
+                            <p className='text-lg mt-5'>Peaceful residences amidst greenery offering sanctuary from city life while maintaining easy urban access.</p>
+                        </div>
+                    </div>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div className='slider-3 slider'>
-                    <div className='overlay'></div>
+                    <div className='overlay flex items-center justify-center'>
+                        <div className='text-white text-center max-w-[700px] mx-auto'>
+                            <h1 className='text-5xl'>Finding Serenity in Every Space</h1>
+                            <p className='text-lg mt-5'>Charming seaside homes with ocean views, sandy beaches, and relaxed coastal living ambiance for ultimate tranquility.</p>
+                        </div>
+                    </div>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div className='slider-4 slider'>
-                    <div className='overlay'></div>
+                    <div className='overlay flex items-center justify-center'>
+                        <div className='text-white text-center max-w-[700px] mx-auto'>
+                            <h1 className='text-5xl'>Discover Your Dream Home Today!</h1>
+                            <p className='text-lg mt-5'>Sustainable residences with green design, eco-friendly materials, and energy-efficient technologies for a greener, more responsible lifestyle.</p>
+                        </div>
+                    </div>
                 </div>
             </SwiperSlide>
         </Swiper>
