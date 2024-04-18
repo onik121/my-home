ab25ffb15222f749b0f806dc2d5ede4f8561c302
hasanonik <jh8539615@gmail.com>
2024-04-18 14:36:19 +0600

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,12 @@ import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 
 const Navbar = () => {
-
+    
     const { user, logOut } = useContext(AuthContext);
 
     const navlinks = <>
         <li><NavLink to={'/'}>Home</NavLink></li>
         <li><NavLink to={'/about'}>About Us</NavLink></li>
-        {/* <li><NavLink to={'/fag'}>Faq</NavLink></li> */}
         {
             user && <li><NavLink to={'/updateprofile'}>Update Profile</NavLink></li>
         }
@@ -28,7 +27,7 @@ const Navbar = () => {
             <div className="navbar max-w-[1440px] mx-auto px-5">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className="lg:hidden mr-3 btn px-3">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[100] p-2 shadow bg-base-100 rounded-box w-52">
