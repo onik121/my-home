229eecd0d2c4142c78e0ba66abf847d085aa92de
hasanonik <jh8539615@gmail.com>
2024-04-18 15:32:59 +0600

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,18 +10,10 @@ const Footer = () => {
                     <p className='mt-3 text-base font-semibold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, suscipit!</p>
                 </aside>
                 <nav>
-                    <h6 className="text-2xl text-black font-semibold mb-2">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav> 
-                    <h6 className="text-2xl text-black font-semibold mb-2">Company</h6>
-                    <a className="link link-hover">About us</a>
+                    <h6 className="text-2xl text-black font-semibold mb-2">Explore</h6>
+                    <a className="link link-hover">Home</a>
+                    <a className="link link-hover">About Us</a>
                     <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
                 </nav>
                 <nav>
                     <h6 className="text-2xl text-black font-semibold mb-2">Legal</h6>
@@ -29,9 +21,15 @@ const Footer = () => {
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
                 </nav>
+                <nav>
+                    <h6 className="text-2xl text-black font-semibold mb-2">Follow</h6>
+                    <a className="link link-hover">Facebook</a>
+                    <a className="link link-hover">Instagram</a>
+                    <a className="link link-hover">Twitter</a>
+                </nav>
             </footer>
             <footer className="footer footer-center p-6 bg-[#31323c] text-white">
-                <p className="text-base">Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+                <p className="text-base">Copyright © 2024 - All right reserved by My Home Industries Ltd.</p>
             </footer>
         </div>
     );
