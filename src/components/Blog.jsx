2624ffc6c350933c6536b8bf4a3b4c43e830567f
hasanonik <jh8539615@gmail.com>
2024-04-18 15:16:05 +0600

    updated blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,8 @@
 
-import img from '../assets/8.jpg'
+import img1 from '../assets/8.jpg'
+import img2 from '../assets/blog-2.jpg'
+import img3 from '../assets/blog-3.jpg'
+
 
 
 const Blog = () => {
@@ -10,20 +13,26 @@ const Blog = () => {
                 <button className="blog">OUR BlOG</button>
             </div>
             <div className='blog-container mt-10'>
-                <div>
-                    <img src={img}></img>
-                    <h1 className='text-xl mt-3 mb-1 font-semibold text-black'>10 Ways to Maximize Your Real Estate Investmente</h1>
-                    <p>December 10, 2024  No Comments</p>
+                <div className='bg-[#f3f4f9]'>
+                    <img className='w-full' src={img1}></img>
+                    <div className='p-5'>
+                        <h1 className='text-[21px] mb-3 font-semibold text-black'>Special Unveiling of 568 N. Tigertail Road</h1>
+                        <p className='text-base'>Our Principal and Partner, Samuel McMillan, recently celebrated the unveiling of 568 N. Tigertail Road, a newly-constructed estate</p>
+                    </div>
                 </div>
-                <div>
-                    <img src={img}></img>
-                    <h1 className='text-xl mt-3 mb-1 font-semibold text-black'>10 Ways to Maximize Your Real Estate Investmente</h1>
-                    <p>December 18, 2024  No Comments</p>
+                <div className='bg-[#f3f4f9]'>
+                    <img className='w-full' src={img2}></img>
+                    <div className='p-5'>
+                        <h1 className='text-[21px] mb-3 font-semibold text-black'>Turks and Caicos Villa Sold for Record $7.6M</h1>
+                        <p className='text-base'>The Agency is pleased to announce the sale of 24 Cherokee Road for $7.625 million—the highest-priced residential sale in Turks and</p>
+                    </div>
                 </div>
-                <div>
-                    <img src={img}></img>
-                    <h1 className='text-xl mt-3 mb-1 font-semibold text-black'>10 Ways to Maximize Your Real Estate Investmente</h1>
-                    <p>December 23, 2024  No Comments</p>
+                <div className='bg-[#f3f4f9]'>
+                    <img className='w-full' src={img3}></img>
+                    <div className='p-5'>
+                        <h1 className='text-[21px] mb-3 font-semibold text-black'>We Build a Better LA for Fifth Year in a Row</h1>
+                        <p className='text-base'>Last week, we partnered with Habitat for Humanity of Greater Los Angeles (Habitat LA) for the fifth year in a row for a build day</p>
+                    </div>
                 </div>
             </div>
         </div>
